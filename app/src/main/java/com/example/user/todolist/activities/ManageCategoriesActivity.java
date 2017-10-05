81a048339c252bb1e1fe97708baaf69f4787f1fc
Raul Ruiz <rauletxailarena@gmail.com>
2017-10-05 13:57:01 +0100

    Style category listview items

--- app/src/main/java/com/example/user/todolist/activities/ManageCategoriesActivity.java
+++ app/src/main/java/com/example/user/todolist/activities/ManageCategoriesActivity.java
@@ -4,14 +4,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.user.todolist.R;
+import com.example.user.todolist.adapter.CategoryAdapter;
 import com.example.user.todolist.category.Category;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ManageCategoriesActivity extends AppCompatActivity {
 
     EditText newCategoryTitle;
+    ArrayList<Category> categories;
+    ListView categoryListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,9 @@ public class ManageCategoriesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_manage_categories);
 
         newCategoryTitle = (EditText) findViewById(R.id.categoryNameEditText);
+        categories = Category.getAllCategories();
+        categoryListView = (ListView) findViewById(R.id.categoryListView);
+        inflateCategoryListView();
     }
 
     public void onCreateCategoryButtonClicked(View view){
@@ -28,5 +38,10 @@ public class ManageCategoriesActivity extends AppCompatActivity {
             category.save();
             newCategoryTitle.setText("");
         }
+    }
+
+    public void inflateCategoryListView(){
+        CategoryAdapter categoryAdapter = new CategoryAdapter(this, categories);
+        categoryListView.setAdapter(categoryAdapter);
     }
 }
